599bcfc67d5bcf1c226545d82141d535a7efba7d
gracehuang <gracehuang@localhost>
2022-09-22 11:44:17 -0500

    added scanner

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,9 +1,11 @@
 package studio3;
+import java.util.Scanner;
 
 public class Sieve {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
 
 	}
 
