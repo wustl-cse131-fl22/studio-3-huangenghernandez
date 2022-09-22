1327e9dedd19261aab30fccd93b88af2996d1919
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-09-22 12:10:23 -0500

    Intial prime check

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -7,13 +7,24 @@ public class Sieve {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		
-		/* Initalize Variables */
+		/* Initialize Variables */
+		boolean isPrime = false;
 		
 		System.out.print("Input number: ");
 		int[] numbers = new int[in.nextInt()];
 		for(int i = 0; i < numbers.length; i++){
+			isPrime = true;
 			numbers[i] = i+1;
-			System.out.println(numbers[i]);
+			
+			if (numbers[i] % 2 == 0 || numbers[i] % 3 == 0 || numbers[i] % 5 == 0 || numbers[i] % 7 == 0 ) {
+				isPrime = false;
+			}
+			if (isPrime) {
+				System.out.println(numbers[i] + " is a prime number!");
+			}
+			
+			//Check for prime
+			//System.out.println(numbers[i]);
 		}
 		
 
