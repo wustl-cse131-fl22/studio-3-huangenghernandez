6e9b1f12e30193dd30435ab43648ce06e1011857
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-09-22 11:48:11 -0500

    Done

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -6,6 +6,12 @@ public class Sieve {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
+		
+		/* Initalize Variables */
+		
+		System.out.print("Input number: ");
+		int[] numbers = new int[in.nextInt()];
+		
 
 	}
 
